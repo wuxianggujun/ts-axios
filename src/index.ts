8fe5cbb74adca45f8f578f915b47e685d595300f
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-03-19 22:47:57 +0800

    buildUrl函数实现

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig) {
+  processConfig(config)
   xhr(config)
 }
 
 export default axios
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformUrl(config)
+}
+function transformUrl(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
